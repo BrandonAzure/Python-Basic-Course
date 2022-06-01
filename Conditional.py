0bfb2bccc72d30550224c45213b7085d71df45bc
Brandon Morales <ceinnova00551@innovaccion.mx>
2022-05-31 22:09:34 -0500

    Initial Commit

---
+++ Conditional.py
@@ -0,0 +1,7 @@
+qualification = input("Enter the rating of AZ-900: ")
+qualification = int(qualification)
+
+if(qualification < 700):
+    print("go for not studying!")
+else :
+    print("Very good!")
---
+++ program.py
@@ -0,0 +1,15 @@
+from tokenize import String
+
+
+name = input("What is your name?: ")
+
+print("Hello "+name)
+
+#String 
+name = ""
+
+age = 28
+
+pi = 3.1416
+
+age = float(age)
